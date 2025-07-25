e798f9d2e6c70aac0fd5662ee0fbcdc6354b5fcf
vedantsrivastava42 <vedantsrivastava42@gmail.com>
2025-07-25 16:23:16 +0530

    week 6 assignment

---
+++ WEEK6/EXERCISE 1/firstreactapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to the 1st Session of React
+    </div>
+  );
+}
+
+export default App;
